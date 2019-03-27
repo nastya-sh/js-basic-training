4c957d6c2d6bc71a02debd0d6cdc5afc1f2c7395
Nastya Shiryaeva <snowdrop10019@gmail.com>
2019-03-27 12:41:36 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//  Задача 1.
+let firstName = "Anastasia", middleName = "Nikolaevna", lastname = "Shiryaeva"
+
+let fullName=`Name: ${lastname}, lastname  ${fn}`;
+let fullName=`Name: ${firstName[0]}, age: ${age}, lastname: ${lastname.length}`;
+
+console.log(oldStant)
+console.log('-------')
+console.log(fullName)
