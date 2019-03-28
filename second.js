6072458c30f55575f7bbfbb085239193851b1d03
Nastya Shiryaeva <snowdrop10019@gmail.com>
2019-03-28 16:55:15 +0400

    fCommit

---
+++ second.js
@@ -0,0 +1,46 @@
+ // Задание 1 Функции
+
+ function x(n) {
+    return n;
+  }
+
+let tempx =x(9);
+console.log(tempx);  
+
+
+
+
+
+//Задание из учебника
+function MakeTea(cups, tea=3) {
+   console.log("Brewing " + cups + " cups of " + tea);
+   }
+
+   let quests = 10;
+
+   MakeTea()
+   console.log('-----')
+   MakeTea('test646')
+   console.log('-----')
+   MakeTea(quests, "Earl Grey"); 
+
+ //Задание 2
+   function z(a, b=100) {
+    return a + b;
+  }
+
+let sum =z(7, 3);
+let sum2 =z(1)
+
+console.log(sum)
+console.log(sum2) 
+
+
+
+//Задание 3
+
+
+
+
+
+
